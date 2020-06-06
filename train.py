df9288a7390c21ddd9c2a803668b22ed26fe2e97
vuvko <notvuvko@gmail.com>
2020-06-06 03:39:37 +0300

    arcface_ft_full

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import argparse
 import pandas as pd
+import albumentations as alb
+import cv2
 import numpy as np
 import mxnet as mx
 import lib
@@ -18,12 +20,37 @@ def config_parser() -> argparse.ArgumentParser:
 def run_train(data_path: Path):
     cfg = BasicConfig(
         seed=444,
-        name='arcface_ft',
+        name='arcface_ft_full',
         num_workers=6,
         gpus=(0,),
         batch_size=32,
-        num_epochs=2,
-        steps=(8, 14, 25, 35, 40, 50, 60, np.inf)
+        num_epochs=10,
+        steps=(3, 5, np.inf),
+        train_augmentations=alb.Compose([
+            alb.OneOf([
+                alb.MotionBlur(blur_limit=5, p=0.3),
+                alb.MedianBlur(blur_limit=3, p=0.2),
+                alb.Blur(blur_limit=5, p=0.2)
+            ], p=0.5),
+            alb.OneOf([
+                alb.ImageCompression(50, compression_type=alb.ImageCompression.ImageCompressionType.JPEG),
+                alb.ImageCompression(50, compression_type=alb.ImageCompression.ImageCompressionType.WEBP)
+            ], p=0.4),
+            alb.Resize(128, 128),
+            alb.OneOf([
+                alb.CLAHE(clip_limit=2),
+                alb.IAASharpen(),
+                alb.IAAEmboss(),
+                alb.RandomBrightnessContrast(),
+            ], p=0.5),
+            alb.Rotate(5, border_mode=cv2.BORDER_REFLECT, p=0.8),
+            alb.OneOf([
+                alb.RandomResizedCrop(112, 112, scale=(0.9, 1.0), ratio=(0.8, 1.1), p=0.9),
+                alb.Resize(112, 112, p=0.1),
+            ], p=1.0),
+            alb.HueSaturationValue(p=0.9),
+            alb.ChannelShuffle(p=0.9)
+        ]),
     )
     np.random.seed(cfg.seed)
     mx.random.seed(cfg.seed)
