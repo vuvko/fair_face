cb6fee6a511f762cb38be37fbb549ce77421443b
vuvko <notvuvko@gmail.com>
2020-06-30 21:38:42 +0300

    Forgot flip.

--- submit_arcface.py
+++ submit_arcface.py
@@ -6,7 +6,8 @@ from lib.submit import submit
 
 
 if __name__ == '__main__':
-    comparator = CompareModel('/home/andrey/.insightface/models/VGG2-ResNet50-Arcface/model', 0, ctx=mx.gpu(0))
+    comparator = CompareModel('/home/andrey/.insightface/models/VGG2-ResNet50-Arcface/model', 0,
+                              ctx=mx.gpu(0), use_flip=True)
     val_dir = Path('/run/media/andrey/Fast/FairFace/test_wide/data')
     val_csv = Path('/run/media/andrey/Fast/FairFace/test/evaluation_pairs/predictions.csv')
     submit_name = 'final_arcface_vgg_flip_allval'
